c215185e8d5e4a0f241cbb944dcb3c39d05d4768
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-15 15:21:17 -0600

    Commit 5 - Sum Array.

--- js/solutions.js
+++ js/solutions.js
@@ -45,8 +45,16 @@ console.log(calculateSide(8, 6));
 
 
 //Sum array
+const sumArray = (arr) => {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	return sum
+};
 
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
-
+	
 
 
